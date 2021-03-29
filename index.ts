1c2dc68f6d04309ed25634430fe41cfa60d73a12
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-29 16:11:33 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,18 +1,23 @@
 // Import stylesheets
 import "./style.css";
 
+interface colorata [
+  colore: string;
+]
 interface miaInterfaccia {
   f1: string,
   f2: any
 }
-class miaClasse implements miaInterfaccia {
+class miaClasse implements miaInterfaccia, colorata {
   f1: string;
   f2: number;
   f3: boolean;
+  colore: string;
   constructor() {
     this.f1 = "";
     this.f2 = 0;
     this.f3 = false;
+    this.colore = "rosso";
   }
 }
 var X = new miaClasse();
